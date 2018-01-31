b8467f6991a824af18cb21b2ee682c1bf42a5464
karelalex <karelalex83@gmail.com>
2018-01-31 14:28:25 +0300

    Big changes in code structure and made ship move to clicked place

--- core/src/ru/naztrans/space/SpaceGame.java
+++ core/src/ru/naztrans/space/SpaceGame.java
@@ -1,23 +1,32 @@
 package ru.naztrans.space;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.graphics.GL20;
+//import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class SpaceGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class SpaceGame extends Game {
+	@Override
+	public void create() {
+	 setScreen(new MenuScreen(this));
+	}
+
+	/*SpriteBatch batch;
+	Texture shipsImg;
 	int x;
 	Texture background;
+	TextureRegion ship;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shipsImg = new Texture("ships.png");
 		background=new Texture("sky.jpg");
-		x=0;
+		ship=new TextureRegion(shipsImg,0,2080,256,278);
+		//x=0;
 	}
 
 	@Override
@@ -25,16 +34,16 @@ public class SpaceGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		x++;
-		if (x>700) {x=-150;}
+		if (x>700) {x=-256;}
 		batch.begin();
 		batch.draw(background,0,0);
-		batch.draw(img, x++, 30);
+		batch.draw(ship, x, 30);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
-	}
+		shipsImg.dispose();
+	}*/
 }
---
+++ engine/src/ru/naztrans/space/engine/Base2DScreen.java
@@ -0,0 +1,93 @@
+package ru.naztrans.space.engine;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by alexk on 31.01.2018.
+ */
+
+public class Base2DScreen implements Screen, InputProcessor {
+    protected Game game;
+    public Base2DScreen(Game game) {
+        this.game=game;
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
