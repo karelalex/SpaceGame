f80ea3d89c050ae80f8419483e5cc4d7e46aa7b5
karelalex <karelalex83@gmail.com>
2018-02-07 16:27:20 +0300

    made hw4

--- core/src/ru/naztrans/space/screen/GameScreen.java
+++ core/src/ru/naztrans/space/screen/GameScreen.java
@@ -3,15 +3,26 @@ package ru.naztrans.space.screen;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import ru.naztrans.space.Background;
 import ru.naztrans.space.engine.Base2DScreen;
+import ru.naztrans.space.engine.math.Rect;
+import ru.naztrans.space.engine.math.Rnd;
+import ru.naztrans.space.star.Star;
 
 /**
  * Created by alexk on 07.02.2018.
  */
 
 public class GameScreen extends Base2DScreen {
-
+    private static final int NUMBER_OF_STARS = 5;
+    private Texture backgroundTexture;
+    private Background background;
+    private TextureAtlas atlas;
+    private Star[] stars;
 
         public GameScreen(Game game) {
             super(game);
@@ -20,6 +31,13 @@ public class GameScreen extends Base2DScreen {
         @Override
         public void show() {
             super.show();
+            backgroundTexture = new Texture("sky.jpg");
+            background = new Background(new TextureRegion(backgroundTexture));
+            atlas = new TextureAtlas("menuAtlas.tpack");
+            stars=new Star[NUMBER_OF_STARS];
+            for(int i=0; i<NUMBER_OF_STARS; i++) {
+                stars[i]=new Star(atlas, Rnd.nextFloat(-0.005f, 0.005f), Rnd.nextFloat(-0.5f, -0.1f), 0.01f);
+            }
         }
 
         @Override
@@ -28,10 +46,22 @@ public class GameScreen extends Base2DScreen {
             Gdx.gl.glClearColor(0.7f, 0.3f, 0.7f, 1);
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
             batch.begin();
+            background.draw(batch);
 
+            for (int i=0; i<NUMBER_OF_STARS; i++)
+            {
+                    stars[i].update(delta);
+                    stars[i].draw(batch);}
             batch.end();
         }
+    @Override
+    protected void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
 
+        for (int i=0; i<NUMBER_OF_STARS; i++)
+        {stars[i].resize(worldBounds);}
+    }
         @Override
         public void dispose() {
             super.dispose();
--- core/src/ru/naztrans/space/screen/MenuScreen.java
+++ core/src/ru/naztrans/space/screen/MenuScreen.java
@@ -24,12 +24,13 @@ public class MenuScreen extends Base2DScreen implements ActionListener{
 
     private static final float BUTTON_HEIGHT = 0.15f;
     private static final float BUTTON_PRESS_SCALE = 0.9f;
+    private static final int NUMBER_OF_STARS = 100;
     private Texture backgroundTexture;
     private Background background;
     private TextureAtlas atlas;
     private ExitButton exit;
     private PlayButton play;
-    private Star star;
+    private Star[] stars;
 
     public MenuScreen(Game game) {
         super(game);
@@ -45,7 +46,10 @@ public class MenuScreen extends Base2DScreen implements ActionListener{
         exit.setHeightProportion(BUTTON_HEIGHT);
         play = new PlayButton(atlas, BUTTON_PRESS_SCALE, this);
         play.setHeightProportion(BUTTON_HEIGHT);
-        star = new Star(atlas, Rnd.nextFloat(-0.005f, 0.005f), Rnd.nextFloat(-0.5f, -0.1f), 0.01f);
+        stars=new Star[NUMBER_OF_STARS];
+        for(int i=0; i<NUMBER_OF_STARS; i++) {
+            stars[i]=new Star(atlas, Rnd.nextFloat(-0.005f, 0.005f), Rnd.nextFloat(-0.5f, -0.1f), 0.01f);
+        }
     }
 
     @Override
@@ -55,16 +59,19 @@ public class MenuScreen extends Base2DScreen implements ActionListener{
         draw();
     }
     public void update(float delta){
-        star.update(delta);
+        for (int i=0; i<NUMBER_OF_STARS; i++)
+        {stars[i].update(delta);}
     }
     public void draw() {
         Gdx.gl.glClearColor(0.7f, 0.3f, 0.7f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
+
+        for (int i=0; i<NUMBER_OF_STARS; i++)
+        {stars[i].draw(batch);}
         exit.draw(batch);
         play.draw(batch);
-        star.draw(batch);
         batch.end();
     }
     @Override
@@ -73,7 +80,8 @@ public class MenuScreen extends Base2DScreen implements ActionListener{
         background.resize(worldBounds);
         exit.resize(worldBounds);
         play.resize(worldBounds);
-        star.resize(worldBounds);
+        for (int i=0; i<NUMBER_OF_STARS; i++)
+        {stars[i].resize(worldBounds);}
     }
 
     @Override
